207e4d19b8aae47e033492c8c3a42f3d9d853c61
aurumnf20 <aurumnf20@gmail.com>
2024-08-05 17:02:41 +0700

    Update NB_PREDIKSI_KELULUSAN_MAHASISWA.py

--- NB_PREDIKSI_KELULUSAN_MAHASISWA.py
+++ NB_PREDIKSI_KELULUSAN_MAHASISWA.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyNTupiky6FSwd1f++AH6nyA",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
